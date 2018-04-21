1b2d05828cb3c5c5df3554d8bda7435c8cd902c8
skandeel <s_kandeel@hotmail.com>
2018-04-20 19:25:50 -0500

    Finished Phase 3

--- program6.cc
+++ program6.cc
@@ -3,16 +3,34 @@
 // CS 3377.501
 
 #include <iostream>
+#include <fstream> // For binary input
+#include <iomanip> // For binary input
+#include <stdint.h> // for uint
 #include "cdk.h"
 
-
-#define MATRIX_WIDTH 4
-#define MATRIX_HEIGHT 3
+#define MATRIX_WIDTH 3
+#define MATRIX_HEIGHT 5
 #define BOX_WIDTH 15
-#define MATRIX_NAME_STRING "Test Matrix"
+#define MATRIX_NAME_STRING "Binary File Contents"
+
+const int maxRecordStringLength = 25;
+
+class BinaryFileRecord{ 
+ public:  
+  uint8_t strLength;
+  char stringBuffer[maxRecordStringLength];
+
+};
+
+class BinaryFileHeader{
+ public:
+  uint32_t magicNumber;
+  uint32_t versionsNumber;
+  uint64_t numRecords;
+};
+
 
 using namespace std;
-
 
 int main()
 {
@@ -29,8 +47,8 @@ int main()
   // values you choose to set for MATRIX_WIDTH and MATRIX_HEIGHT
   // above.
 
-  const char 		*rowTitles[] = {"R0", "R1", "R2", "R3", "R4", "R5"};
-  const char 		*columnTitles[] = {"C0", "C1", "C2", "C3", "C4", "C5"};
+  const char 		*rowTitles[] = {"a", "a", "b", "c", "d", "e"};
+  const char 		*columnTitles[] = {"a", "a", "b", "c"};
   int		boxWidths[] = {BOX_WIDTH, BOX_WIDTH, BOX_WIDTH, BOX_WIDTH, BOX_WIDTH, BOX_WIDTH};
   int		boxTypes[] = {vMIXED, vMIXED, vMIXED, vMIXED,  vMIXED,  vMIXED};
 
@@ -52,7 +70,7 @@ int main()
 			  MATRIX_NAME_STRING, (char **) rowTitles, (char **) columnTitles, boxWidths,
 				     boxTypes, 1, 1, ' ', ROW, true, true, false);
 
-  if (myMatrix ==NULL)
+  if (myMatrix == NULL)
     {
       printf("Error creating Matrix\n");
       _exit(1);
@@ -61,11 +79,45 @@ int main()
   /* Display the Matrix */
   drawCDKMatrix(myMatrix, true);
 
-  /*
-   * Dipslay a message
-   */
-  setCDKMatrixCell(myMatrix, 2, 2, "Test Message");
-  drawCDKMatrix(myMatrix, true);    /* required  */
+  // Get input from binary file 
+  ifstream binInfile("cs3377.bin", ios::in | ios::binary);  
+ 
+  BinaryFileHeader *myHeader = new BinaryFileHeader();
+  BinaryFileRecord *myRecord = new BinaryFileRecord();
+
+  // Reading in header contents 
+  binInfile.read((char *) myHeader, sizeof(BinaryFileHeader));
+
+  // Creating a buffer to transfer information
+  char input[maxRecordStringLength];
+
+  // Filling in matrix  
+
+  sprintf(input, "Magic: %X", myHeader->magicNumber);
+  setCDKMatrixCell(myMatrix, 1, 1, input);
+
+  sprintf(input, "Versions: %d", myHeader->versionsNumber);
+  setCDKMatrixCell(myMatrix, 1, 2, input);
+
+  sprintf(input, "NumRecords: %d", myHeader->numRecords);
+  setCDKMatrixCell(myMatrix, 1, 3, input);
+ 
+  setCDKMatrixCell(myMatrix, 2, 1, "strlen: ");
+  setCDKMatrixCell(myMatrix, 2, 2, "");
+
+  setCDKMatrixCell(myMatrix, 3, 1, "strlen: ");
+  setCDKMatrixCell(myMatrix, 3, 2, "");
+
+  setCDKMatrixCell(myMatrix, 4, 1, "strlen: ");
+  setCDKMatrixCell(myMatrix, 4, 2, "");
+
+  setCDKMatrixCell(myMatrix, 5, 1, "strlen: ");
+  setCDKMatrixCell(myMatrix, 5, 2, "");
+ 
+  drawCDKMatrix(myMatrix, true);
+
+  // close file input
+  binInfile.close();
 
   /* So we can see results, pause until a key is pressed. */
   unsigned char x;
